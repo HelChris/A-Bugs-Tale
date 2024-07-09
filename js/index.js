fbe17f395e01190dfd544d40c143a52b82e21b7d
HelChris <111705707+HelChris@users.noreply.github.com>
2024-07-09 21:45:57 +0200

    added theme switch and focus-visible

--- js/index.js
+++ js/index.js
@@ -1,4 +1,29 @@
 import { makeApiCall } from "/js/api/makeApiCall.mjs";
+import { switchTheme } from "./ui/shared/switchTheme.mjs";
+import { applySavedTheme } from "./ui/shared/switchTheme.mjs";
 
 makeApiCall();
 
+// Call applySavedTheme when the document is loaded
+document.addEventListener("DOMContentLoaded", applySavedTheme);
+
+applySavedTheme();
+
+//add event listener for the theme switcher
+document
+	.querySelector("#theme-switcher")
+	.addEventListener("click", switchTheme);
+
+document.addEventListener("DOMContentLoaded", function () {
+	var themeSwitcher = document.getElementById("theme-switcher");
+
+	themeSwitcher.addEventListener("keydown", function (event) {
+		//chech if enter or spaced is pressed
+		if (event.key === "Enter" || event.key === " ") {
+			//prevent the deafult action to avoid scrolling the page when pressing space
+			event.preventDefault();
+			//toggle the theme
+			switchTheme();
+		}
+	});
+});
